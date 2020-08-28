4b95807de494b4f6f12ecbc26d72e196813a89af
Muhammad Faris <muhfaris@disroot.org>
2020-08-28 15:56:54 +0700

    feat: substraction two number

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import "log"
 func main() {
 	log.Println("MAIN APP")
 	calculate(9, 1)
+	sub(10, 9)
 }
 
 func calculate(x, i int) {
 	log.Println(x + i)
+}
+
+func sub(x, i int) {
+	log.Println(x - i)
 }
