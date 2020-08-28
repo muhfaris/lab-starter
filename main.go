608271c9992bd3c801d8055b733ccfedbf784bf0
Muhammad Faris 'Afif <muhfaris@disroot.org>
2020-08-28 22:46:50 +0700

    feat: Create and Read User API (#3)
    * feat: add user api
    
    * feat: add Read user api

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import "log"
 
 func main() {
-
 	num := calculate(9, 1)
 	log.Println(num)
 
 	num2 := sub(10, 9)
 	log.Println(num2)
+
+	xx := calculate(num, num2)
+	log.Println(xx)
 }
 
 func calculate(x, i int) int {
